1d3bab155ca6d26453c965b0ec65495bf2960e91
Dave <dave@westburydigital.com.au>
2022-10-02 00:00:59 +0930

    moved header from beach.php to db_singlebeach.php

--- beach.php
+++ beach.php
@@ -16,79 +16,8 @@
     gtag('config', 'G-659MYBSRDE');
     </script>
 
-    <title>YORKES.LIVE | Yorke Peninsula's Best Beach List</title>
-    <meta charset="UTF-8">
-    <meta name="description"
-        content="An interactive map of the Yorke Peninsula's best beaches. As voted on by you. Come explore each beach on the Yorke Peninsula with locations, pictures, access and attractions.">
-    <meta name="keywords" content="yorkes yorke peninsula beaches map best surf">
-    <meta name="author" content="Written by D.W. Hills, Length: 1 pages">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <meta property="og:url" content="https://yorkes.live" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Yorkes.Live" />
-    <meta property="og:description" content="An interactive map of the Yorke Peninsula's best beaches. As voted on by you." />
-    <meta property="og:image" content="https://yorkes.live/fb.png" />
-
-    <link type="text/css" href="./styles.css" rel="stylesheet" />
-
-    <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-
-    <!-- MODEL VIEWER -->
-    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
-
-    <!-- BOOTSTRAP CSSs -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous">
-
-    <!-- FONTAWESOME -->
-    <link href="./fontawesome/css/fontawesome.css" rel="stylesheet">
-    <link href="./fontawesome/css/brands.css" rel="stylesheet">
-    <link href="./fontawesome/css/solid.css" rel="stylesheet">
-
-    <!-- JQUERY -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 
-    <!-- BOOTSTRAP JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous">
-    </script>
-
-</head>
-
-<body data-bs-spy="scroll" data-bs-target="#navbarID">
-    <nav id="nav-color" class="navbar fixed-top navbar-expand-sm navbar-dark border-bottom border-3">
-
-        <a class="navbar-brand text-light" href="./index.php">
-            <h4 id="nav-height" class="d-flex align-content-center flex-wrap"><span id="brandbox" class="align-middle pt-2 ps-4">YORKES<span class="text-orange">.</span>LIVE</span></h4>
-        </a>
-        <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarID" aria-controls="navbarID" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon "></span>
-        </button>
-        <div id="navbarID" class="collapse navbar-collapse">
-            <div class="navbar-nav" role="">
-                <a class="nav-link nav-item nav-text-light ps-2" aria-current="page" href="./index.php"><span class="align-middle">Map</span></a>
-                <a class="nav-link nav-item nav-text-light ps-2" aria-current="page" href="./index.php#mainlike"><span class="align-middle">Beaches</span></a>
-                <a class="nav-link nav-item nav-text-light ps-2" aria-current="page" href="./index.php#info"><span class="align-middle">Info</span></a>
-                <a class="nav-link nav-item  nav-text-light ps-2" aria-current="page" href=".index.php#contact"><span class="align-middle">Contact</span></a>
-            </div>
-            <ul class="navbar-nav  ms-auto">
-                <li class="nav-item d-flex">
-                    <a class="nav-link text-light " aria-current="page" href="#beaches">
-                        <h4><span id="coins" class="align-middle text-blue p-2 "><?php include "db_getCoins.php"?></span></h4>
-                    </a>
-                    <img id="" class="me-4" src="img/shakawhite.png" width="45px" height="45px" alt="Shaka Icon">
-                </li>
-            </ul>
-
-        </div>
-    </nav>
-    <div id="navbumper" class="bg-rich" style="height: 120px"></div>
     <!-- START OF PHP  -->
 
     <?php include "db_singlebeach.php" ?>
--- db_singlebeach.php
+++ db_singlebeach.php
@@ -120,148 +120,224 @@ if($connectToServer)
             }
         }
 
-           echo "    <div id='beachlander' class='bg-rich'>
-           <div class='text-light'>
-               <div class='row gx-0'>
-                   <div class='mainlike-left col-6 bg-rich d-none d-sm-none d-md-block ' style='position:relative'>
-                       <p class='display-4 text-center feed-your-soul ps-4'>" . $row['spelt_name'] . "</p>
-                       <div class='vr'></div>
-                       <p class='sideways-text lead h-100'>Beach vibes <span class='squish'>-----------------------</span></p>
-                   </div>
-                   <div class='mainlike-right col-md-6 bg-rich text-center'>
-                        <p class='display-4 text-center feed-your-soul ps-4 d-md-none'>" . $row['spelt_name'] . "</p>
-                        <div class='overflow-hidden'>
-                            <picture>
-                                <source srcset='./img/big/" . $row["name"]. ".webp'>
-                                <img src='./img/big/" . $row["title_image"]. "' class='img-fluid' style='max-width: 400px;' alt='Image of " . $row["spelt_name"]. " '>
-                            </picture>
-                        </div>
-                   </div>
-               </div>
-               <div class='row'>
-                   <div class='col-12 text-center'>
-                       <h5 class='h3 feed-your-soul py-2'>Where the sky touches the sea<span class='text-orange'>.</span> </h5>
-                   </div>
-               </div>
-           </div>
-       </div>
-
-       <div id='bg-waves'></div>
-
-       <div class='container'>
-           <div class='row my-3'>
-               <div class='col-md-8'>
-                   <div class='col-12 border-rich p-3 bg-light h-100'>
-                       <h3 class='text-uppercase py-3'>More information about " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
-                       <p>" . $row['big_write_up'] . "</p>
-                       <h3 class='text-uppercase py-3'>History about " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
-                       <p>" . $row['history'] . "</p>
-                     
-                   </div>
-               </div>
-               <div class='col-md-4'>
-                    <div class='col-12'>
-                        <div class='col-12'>
-                            <div class='row text-center py-3 '>
-                                    " . $iconsOutput . "
-                            </div>
-                        </div>
-                        <div class='col-12 border-rich bg-light text-center p-3'>
-                            <h3 class='text-uppercase'>Alternate names for " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
-                            <p>" . $row['alt_names'] . "</p>
-                        </div>
-                        <div class='col-12 bg-light border-rich p-3 mt-3'>
-                            <h3 class='text-center text-uppercase'>Surf Report<span class='text-orange'>.</span></h3>
-                            <p>" . $row["surf"] . "</p>
-                        </div>
-                    </div>                       
-                </div>
-           </div> 
-         
-
-           <div class='row '>
-               <div class='col-md-6 my-2'>
-                   <div class='col-12 border-rich bg-light p-3 h-100'>
-                       <h3 class='text-center  text-uppercase'>Camping near " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
-
-                       <ul>
-                            " . $row["camping"] . "
-                       </ul>
-                   </div>
-               </div>
-               <div class='col-md-6 my-2'>
-                   <div class='col-12 border-rich bg-light p-3 h-100'>
-                       <h3 class='text-center text-uppercase'>Accommodation near " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
-                       <ul>
-                            " . $row["accommodation"] . "
-                       </ul>
-                   </div>
-               </div>
-           </div>
+           echo "    
+           <title>YORKES.LIVE | " . $row['spelt_name'] .  " </title>
+           <meta charset='UTF-8'>
+           <meta name='description' content='This is " . $row['spelt_name'] .  " on the sunny Yorke Peninsula. Come learn more about the beach and what it has to offer. Don't forget to vote for  " . $row['spelt_name'] .  " . '>
+           <meta name='keywords' content='yorkes " . $row['spelt_name'] .  " yorke peninsula beaches map best surf'>
+           <meta name='author' content='Written by D.W. Hills, Length: 1 pages'>
+           <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+       
+           <meta property='og:url' content='https://yorkes.live' />
+           <meta property='og:type' content='website' />
+           <meta property='og:title' content='Yorkes.Live' />
+           <meta property='og:description' content='An interactive map of the Yorke Peninsula&apos;s best beaches. As voted on by you.' />
+           <meta property='og:image' content='https://yorkes.live/fb.png' />
+       
+           <link type='text/css' href='./styles.css' rel='stylesheet' />
+       
+           <link rel='apple-touch-icon' sizes='180x180' href='./apple-touch-icon.png'>
+           <link rel='icon' type='image/png' sizes='32x32' href='./favicon-32x32.png'>
+           <link rel='icon' type='image/png' sizes='16x16' href='./favicon-16x16.png'>
+           <link rel='manifest' href='/site.webmanifest'>
+       
+           <!-- MODEL VIEWER -->
+           <script type='module' src='https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js'></script>
+       
+           <!-- BOOTSTRAP CSSs -->
+           <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC'
+               crossorigin='anonymous'>
+       
+           <!-- FONTAWESOME -->
+           <link href='./fontawesome/css/fontawesome.css' rel='stylesheet'>
+           <link href='./fontawesome/css/brands.css' rel='stylesheet'>
+           <link href='./fontawesome/css/solid.css' rel='stylesheet'>
+       
+           <!-- JQUERY -->
+           <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
+       
+       
+           <!-- BOOTSTRAP JS -->
+           <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM'
+               crossorigin='anonymous'>
+           </script>
+       
+       </head>
            
+<body data-bs-spy='scroll' data-bs-target='#navbarID'>
+<nav id='nav-color' class='navbar fixed-top navbar-expand-sm navbar-dark border-bottom border-3'>
+
+    <a class='navbar-brand text-light' href='./index.php'>
+        <h4 id='nav-height' class='d-flex align-content-center flex-wrap'><span id='brandbox' class='align-middle pt-2 ps-4'>YORKES<span class='text-orange'>.</span>LIVE</span></h4>
+    </a>
+    <button class='navbar-toggler text-light' type='button' data-bs-toggle='collapse' data-bs-target='#navbarID' aria-controls='navbarID' aria-expanded='false'
+        aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon '></span>
+    </button>
+    <div id='navbarID' class='collapse navbar-collapse'>
+        <div class='navbar-nav' role=''>
+            <a class='nav-link nav-item nav-text-light ps-2' aria-current='page' href='./index.php'><span class='align-middle'>Map</span></a>
+            <a class='nav-link nav-item nav-text-light ps-2' aria-current='page' href='./index.php#mainlike'><span class='align-middle'>Beaches</span></a>
+            <a class='nav-link nav-item nav-text-light ps-2' aria-current='page' href='./index.php#info'><span class='align-middle'>Info</span></a>
+            <a class='nav-link nav-item  nav-text-light ps-2' aria-current='page' href='.index.php#contact'><span class='align-middle'>Contact</span></a>
         </div>
-        <div class='container-fluid'>
-           <div class='col-12'>
-               <h3 class='text-center my-3 text-uppercase'>Image Gallery of " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
-           </div>
-           <div id='gallery' class='row' data-masonry='{'percentPosition': true }'>
+        <ul class='navbar-nav  ms-auto'>
+            <li class='nav-item d-flex'>
+                <a class='nav-link text-light ' aria-current='page' href='#beaches'>
+                    <h4><span id='coins' class='align-middle text-blue p-2 '><?php include 'db_getCoins.php'?></span></h4>
+</a>
+<img id='' class='me-4' src='img/shakawhite.png' width='45px' height='45px' alt='Shaka Icon'>
+</li>
+</ul>
 
-                " . $galleryOutput . "
-                   
-               </div>
-           </div>
-           <h5 class='h3 feed-your-soul py-2 text-center'>" . $extraText . "Have you taken a photo you would love to <a href='http://yorkes.live/contact'>submit?</a></h5>
+</div>
+</nav>
+<div id='navbumper' class='bg-rich' style='height: 120px'></div>
+<div id='beachlander' class='bg-rich'>
+    <div class='text-light'>
+        <div class='row gx-0'>
+            <div class='mainlike-left col-6 bg-rich d-none d-sm-none d-md-block ' style='position:relative'>
+                <p class='display-4 text-center feed-your-soul ps-4'>" . $row['spelt_name'] . "</p>
+                <div class='vr'></div>
+                <p class='sideways-text lead h-100'>Beach vibes <span class='squish'>-----------------------</span></p>
+            </div>
+            <div class='mainlike-right col-md-6 bg-rich text-center'>
+                <p class='display-4 text-center feed-your-soul ps-4 d-md-none'>" . $row['spelt_name'] . "</p>
+                <div class='overflow-hidden'>
+                    <picture>
+                        <source srcset='./img/big/" . $row["name"]. ".webp'>
+                        <img src='./img/big/" . $row["title_image"]. "' class='img-fluid' style='max-width: 400px;' alt='Image of " . $row["spelt_name"]. " '>
+                    </picture>
+                </div>
+            </div>
         </div>
-        <div class='container'>
-           <div class='row py-4'>
-               <div class='col-md-4 mb-3 order-md-first order-last'>
-                   <div class='col-12 text-center border-rich bg-light h-100 py-3' style='min-height: 600px;'>
-                       <h3 class='text-uppercase'>Instagram Posts<span class='text-orange'>.</span></h3>
-                       " . $row["instagram"] . "
-                       <small class='feed-your-soul py-2 text-center'><a href='http://yorkes.live/contact'>Submit</a> your instgram posts to this section.</small>
+        <div class='row'>
+            <div class='col-12 text-center'>
+                <h5 class='h3 feed-your-soul py-2'>Where the sky touches the sea<span class='text-orange'>.</span> </h5>
+            </div>
+        </div>
+    </div>
+</div>
 
-                   </div>
-               </div>
-               <div class='col-md-8 mb-3 '>
-                   <div class='col-12 text-center border-rich bg-light p-0 h-100'>
-                       
-                       " . $row["googlemap"] . "
-                       
-                   </div>
-               </div>
-           </div>
-           <div class='row mb-4'>
-               <div class='col-12'>
-                   <div class='col-12 border-rich bg-light p-3'>
-                       <h3 class='text-center text-uppercase'>Similar Beaches<span class='text-orange'>.</span></h3>
-                       <p class='text-center'>If you liked this beach, you might also like these. </p>
-                       <ul>
-                           <li>
-                                <a href='https://yorkes.live/beach.php?location=flahertys'><span class='fw-bold'>Flaherty's Beach</span> - Beautiful big open beach perfect for pulling up a chair under your 4WD awning.</a>
-                           </li>
-                           <li>
-                                <a href='https://yorkes.live/beach.php?location=browns'><span class='fw-bold'>Brown's Beach</span> - Deep in Dhilba Guuranda-Innes National Park fantastic beach for fishing and walking.</a>
-                           </li>
-                           <li>
-                                <a href='https://yorkes.live/beach.php?location=shell'><span class='fw-bold'>Shell Beach</span> - Grab the kids and check out Shell Beach perfect camping near by.</a>
-                           </li>
-                           <li>
-                                <a href='https://yorkes.live/beach.php?location=troubridge'><span class='fw-bold'>Troubridge</span> - Keep out of the wind and maybe catch a wave.</a>
-                           </li>
-                       </ul>
-                   </div>
-               </div>
-           </div>
-       </div>";
+<div id='bg-waves'></div>
+
+<div class='container'>
+    <div class='row my-3'>
+        <div class='col-md-8'>
+            <div class='col-12 border-rich p-3 bg-light h-100'>
+                <h3 class='text-uppercase py-3'>More information about " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
+                <p>" . $row['big_write_up'] . "</p>
+                <h3 class='text-uppercase py-3'>History about " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
+                <p>" . $row['history'] . "</p>
+
+            </div>
+        </div>
+        <div class='col-md-4'>
+            <div class='col-12'>
+                <div class='col-12'>
+                    <div class='row text-center py-3 '>
+                        " . $iconsOutput . "
+                    </div>
+                </div>
+                <div class='col-12 border-rich bg-light text-center p-3'>
+                    <h3 class='text-uppercase'>Alternate names for " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
+                    <p>" . $row['alt_names'] . "</p>
+                </div>
+                <div class='col-12 bg-light border-rich p-3 mt-3'>
+                    <h3 class='text-center text-uppercase'>Surf Report<span class='text-orange'>.</span></h3>
+                    <p>" . $row["surf"] . "</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-    
-        }
-    } else {
-        echo "<h3 class='p-4'>ERROR:</h3><p class='ps-4'>There is something wrong with the URL or the database. <a href='./index.php'>Try this link.</a></p>";
-    }
-        
-        mysqli_close($connectToServer);
-    }
+    <div class='row '>
+        <div class='col-md-6 my-2'>
+            <div class='col-12 border-rich bg-light p-3 h-100'>
+                <h3 class='text-center  text-uppercase'>Camping near " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
+
+                <ul>
+                    " . $row["camping"] . "
+                </ul>
+            </div>
+        </div>
+        <div class='col-md-6 my-2'>
+            <div class='col-12 border-rich bg-light p-3 h-100'>
+                <h3 class='text-center text-uppercase'>Accommodation near " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
+                <ul>
+                    " . $row["accommodation"] . "
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class='container-fluid'>
+    <div class='col-12'>
+        <h3 class='text-center my-3 text-uppercase'>Image Gallery of " . $row["spelt_name"] . "<span class='text-orange'>.</span></h3>
+    </div>
+    <div id='gallery' class='row' data-masonry='{' percentPosition': true }'>
+
+        " . $galleryOutput . "
+
+    </div>
+</div>
+<h5 class='h3 feed-your-soul py-2 text-center'>" . $extraText . "Have you taken a photo you would love to <a href='http://yorkes.live/contact'>submit?</a></h5>
+</div>
+<div class='container'>
+    <div class='row py-4'>
+        <div class='col-md-4 mb-3 order-md-first order-last'>
+            <div class='col-12 text-center border-rich bg-light h-100 py-3' style='min-height: 600px;'>
+                <h3 class='text-uppercase'>Instagram Posts<span class='text-orange'>.</span></h3>
+                " . $row["instagram"] . "
+                <small class='feed-your-soul py-2 text-center'><a href='http://yorkes.live/contact'>Submit</a> your instgram posts to this section.</small>
+
+            </div>
+        </div>
+        <div class='col-md-8 mb-3 '>
+            <div class='col-12 text-center border-rich bg-light p-0 h-100'>
+
+                " . $row["googlemap"] . "
+
+            </div>
+        </div>
+    </div>
+    <div class='row mb-4'>
+        <div class='col-12'>
+            <div class='col-12 border-rich bg-light p-3'>
+                <h3 class='text-center text-uppercase'>Similar Beaches<span class='text-orange'>.</span></h3>
+                <p class='text-center'>If you liked this beach, you might also like these. </p>
+                <ul>
+                    <li>
+                        <a href='https://yorkes.live/beach.php?location=flahertys'><span class='fw-bold'>Flaherty's Beach</span> - Beautiful big open beach perfect for pulling up a chair
+                            under your 4WD awning.</a>
+                    </li>
+                    <li>
+                        <a href='https://yorkes.live/beach.php?location=browns'><span class='fw-bold'>Brown's Beach</span> - Deep in Dhilba Guuranda-Innes National Park fantastic beach for
+                            fishing and walking.</a>
+                    </li>
+                    <li>
+                        <a href='https://yorkes.live/beach.php?location=shell'><span class='fw-bold'>Shell Beach</span> - Grab the kids and check out Shell Beach perfect camping near by.</a>
+                    </li>
+                    <li>
+                        <a href='https://yorkes.live/beach.php?location=troubridge'><span class='fw-bold'>Troubridge</span> - Keep out of the wind and maybe catch a wave.</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>";
+
+
+
+}
+} else {
+echo "<h3 class='p-4'>ERROR:</h3>
+<p class='ps-4'>There is something wrong with the URL or the database. <a href='./index.php'>Try this link.</a></p>";
+}
+
+mysqli_close($connectToServer);
+}
 
 ?>
